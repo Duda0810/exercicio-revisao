413afd1b4fa2c57b6672c6c6e1621ef37d9db8f2
Duda0810 <164265885+Duda0810@users.noreply.github.com>
2024-05-03 21:42:46 -0300

    Add files via upload

---
+++ aula03-05/index.js
@@ -0,0 +1,58 @@
+const anoNascimento = 2008
+const anoAtual = 2024
+let idade = anoNascimento - anoAtual;
+console.log(idade)
+
+const numero1 = 23
+const numero2 = 70
+const soma = 93
+const subtraçao = -47
+const multiplicaçao = 1610
+const divisao = 0.328
+const modulo = 23
+
+console.log(numero1+numero2)
+console.log(numero1-numero2)
+console.log(numero1*numero2)
+console.log(numero1/numero2)
+console.log(numero1%numero2)
+
+//comparadores
+const Numero1 = 19
+const Numero2 = 35
+//igual = ===
+console.log(numero1 === numero2)
+//diferente !==
+console.log(numero1 !== numero2)
+//maior que >
+console.log(numero1>numero2)
+//maior ou igual que >=
+console.log(numero1 >= numero2)
+//menor que <
+console.log(numero1 < numero2)
+//menor ou igual que <=
+console.log(numero1 <= numero2)
+
+
+//operadore lógicos
+let temIidadeParaDirigir = 19
+let temCarteiraDeMotorista = 'nao'
+let temCarroPróprio = 'nao'
+
+if (temIidadeParaDirigir >= 18)
+    console.log("Você pode dirigir legalmente!")
+ else (temCarteiraDeMotorista === "nao")
+    console.log("Você nao pode dirigir!")
+
+
+const nome = "  Maria,  Verinha,  Laura   "
+
+function toUpperCase(){
+     nome
+}
+toUpperCase()
+
+
+nome.Trim()
+nome.includes("a")
+nome.replaceAll("a","#")
